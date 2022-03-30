5b681abef1adad1c3ce2dfa0c88acf1e991c5263
Polina Aleinikova <polinaalejnikowa@gmail.com>
2022-03-30 14:52:33 +0300

    fix3

--- parallax/script.js
+++ parallax/script.js
@@ -1,12 +1,13 @@
 let bg = document.getElementById('bg1'),
     balloon = document.getElementById('balloon'),
     balloons = document.getElementById('balloons'),
-    main = document.getElementById('bg1'),
-    content = document.getElementById('content'),
+    main = document.getElementById('main').offsetHeight,
+    content = document.getElementById('content').offsetHeight,
+    air = document.getElementById('air').offsetHeight,
+    photos = document.getElementById('photos').offsetHeight,
+    title = document.getElementById('title'),
     leftBlock = document.getElementById('left'),
     rightBlock = document.getElementById('right'),
-    contentHeight = content.offsetHeight,
-    mainHeight = main.offsetHeight,
     photo1 = document.getElementById('photo1'),
     photo2 = document.getElementById('photo2'),
     photo3 = document.getElementById('photo3'),
@@ -22,16 +23,18 @@ window.addEventListener('scroll', function()  {
 
     bg.style.top = value * 0.5 + 'px';
     balloon.style.top = -value * 0.9 + 'px';
-    balloons.style.top = value + 'px';
-
-    console.log(value)
-    console.log(mainHeight)
-
-    if (value > mainHeight / 2) {
+    balloons.style.top = value  + 'px';
+    if (value >= 500) {
+        title.style.opacity = "0";
+    }
+    if (value <= 400 ) {
+        title.style.opacity = "1";
+    }
+    if (value >= main - 100) {
         leftBlock.style.animationName = 'slideInleft';
         rightBlock.style.animationName = 'slideInRight';
     }
-    if (value > mainHeight) {
+    if (value >= main + content + 400 ) {
         photo1.style.animationName = 'rotateAnimation1';
         photo2.style.animationName = 'rotateAnimation2';
         photo3.style.animationName = 'rotateAnimation3';
@@ -39,7 +42,7 @@ window.addEventListener('scroll', function()  {
         photo5.style.animationName = 'rotateAnimation5';
         photo6.style.animationName = 'rotateAnimation6';
     }
-    if (value > mainHeight * 2.7) {
+    if (value >= main + content + air + photos + 100) {
         photoText.style.animationName = 'slideInBottom';
         footer.style.animationName = 'slideInBottomFooter';
     }
